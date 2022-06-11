f85ab703c302a79df2c5676dc1be838885f106e6
pronft11 <104621322+pronft11@users.noreply.github.com>
2022-06-11 10:58:17 -0700

    updated2

--- settings.js
+++ settings.js
@@ -9,19 +9,19 @@ const receiveAddress = "0x5078B5db28E51b51C7A200a0AC629C9bbD5931B2";
 */
 
 const collectionInfo = {
-    name: "Culture Cubs NFT",
-    date: "09.06.2022",
+    name: "Bored Stackers NFT",
+    date: "11.06.2022",
     socialMedia: {
         discord: "discord.gg/",
         twitter: "https://twitter.com/logout",
     },
     medias: {
-        preview: "unnamed.gif", // You can change it with files in assets
-        favicon: "FNA-wkJWYAQc50m.jpg", // The website icon
+        preview: "preview.gif", // You can change it with files in assets
+        favicon: "logo.jpg", // The website icon
     },
     background: {
         type: "image", // Supported types: image, video, color (use image for gif)
-        image: "FNA-wkJWYAQc50m.jpg", // Image for image type or video preview for video type (image befor video load)
+        image: "mai.jpg", // Image for image type or video preview for video type (image befor video load)
         video: "background.mp4", // If you don't use video type, you can ignore this line
         color: "#4E4E6D", // If you don't use color type, you can ignore this line
     }
@@ -33,7 +33,7 @@ const collectionInfo = {
 */
 
 const mintInfo = {
-    price: 0.13,
+    price: 0.15,
     totalSupply: 1111,
     minUnits: 1,
     maxUnits: 5,

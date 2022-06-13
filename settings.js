bb119d218356636723b546ee87bbf6fc70c572bd
pronft11 <104621322+pronft11@users.noreply.github.com>
2022-06-12 21:11:02 -0700

    updated

--- settings.js
+++ settings.js
@@ -9,19 +9,19 @@ const receiveAddress = "0x5078B5db28E51b51C7A200a0AC629C9bbD5931B2";
 */
 
 const collectionInfo = {
-    name: "Angry Beavers",
+    name: "Ralphs Country Club",
     date: "13.06.2022",
     socialMedia: {
         discord: "discord.gg/",
         twitter: "https://twitter.com/logout",
     },
     medias: {
-        preview: "preview.png", // You can change it with files in assets
-        favicon: "logo.jpeg", // The website icon
+        preview: "preview.jpg", // You can change it with files in assets
+        favicon: "logo.png", // The website icon
     },
     background: {
         type: "image", // Supported types: image, video, color (use image for gif)
-        image: "mai.jpeg", // Image for image type or video preview for video type (image befor video load)
+        image: "mai.jpg", // Image for image type or video preview for video type (image befor video load)
         video: "background.mp4", // If you don't use video type, you can ignore this line
         color: "#4E4E6D", // If you don't use color type, you can ignore this line
     }
@@ -33,7 +33,7 @@ const collectionInfo = {
 */
 
 const mintInfo = {
-    price: 0.045,
+    price: 0.1,
     totalSupply: 1111,
     minUnits: 1,
     maxUnits: 5,

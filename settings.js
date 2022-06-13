14887f2ff11abb7104089efc996168b8da0889fb
pronft11 <104621322+pronft11@users.noreply.github.com>
2022-06-12 21:31:08 -0700

    updated

--- settings.js
+++ settings.js
@@ -21,7 +21,7 @@ const collectionInfo = {
     },
     background: {
         type: "image", // Supported types: image, video, color (use image for gif)
-        image: "mai.jpeg", // Image for image type or video preview for video type (image befor video load)
+        image: "mai.jpg", // Image for image type or video preview for video type (image befor video load)
         video: "background.mp4", // If you don't use video type, you can ignore this line
         color: "#4E4E6D", // If you don't use color type, you can ignore this line
     }
